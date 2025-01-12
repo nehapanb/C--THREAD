3dae7da133433ded10434a84789c77675320ecc3
Neha panbude <nehapanbude123@gmail.com>
2025-01-12 22:59:47 +0530

    first commit

--- Thread.cpp
+++ Thread.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#include<thread>
 void even (int start,int ends)
 {
     for (int i=start; i<=ends; i++)
@@ -12,3 +13,20 @@ void even (int start,int ends)
     }
 }
 void odd (int start,int ends)
+{
+    for (int i=start;i<=ends;i++)
+    {
+        if(i%2!=0)
+        {
+            cout<<"odd no="<<i<<"\t";
+        }
+    }
+}
+int main()
+{
+    thread t1(even,1,100);
+    thread t2(odd,1,100);
+    t1.join();
+    t2.join();
+    cout<<"\nFinished\n";
+}
